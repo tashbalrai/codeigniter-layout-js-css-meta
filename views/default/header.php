db87cbe49ae16e8d46b14c85b6cdef845de863f0
Vipan Balrai <vipan.balrai@gmail.com>
2016-04-29 17:45:18 +0530

    removed unwanted code.

--- views/default/header.php
+++ views/default/header.php
@@ -14,10 +14,8 @@
 		?>
 		<!-- Meta end -->
 		<!-- Include CSS -->
-			<link rel="icon" type="image/png" href="images/fav.png">
-			<link href="<?php echo get_layout_url('css')?>/style.css" media="screen" rel="stylesheet"/>
-			<link href="<?php echo get_layout_url('css')?>/style-step.css" media="screen" rel="stylesheet"/>
-			<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<link rel="icon" type="image/png" href="images/fav.png">
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 		<!-- Include dynamic CSS start -->
 		<?php 
 			foreach($css as $l):
@@ -29,16 +27,6 @@
 			endforeach;
 		?>
 		<!-- CSS end -->
-		<!-- Include JS script -->
-			<script src="<?php echo get_layout_url('js')?>/jquery-1.11.3.min.js" type="text/javascript"></script>
-			<script src="<?php echo get_layout_url('js')?>/common.js" type="text/javascript"></script>
-			<script src="<?php echo get_layout_url('js')?>/yav/yav-config.js" type="text/javascript"></script>
-			<script src="<?php echo get_layout_url('js')?>/yav/yav.js" type="text/javascript"></script>
-			<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-			<script type="text/javascript">
-				var js_base_url = "<?php echo base_url(); ?>";
-				var currency_sign = "<?php echo CURRENCY_SIGN; ?>";
-			</script>
 		<!-- Include dynamic JS start -->
 		<?php
 			foreach($js as $s):
